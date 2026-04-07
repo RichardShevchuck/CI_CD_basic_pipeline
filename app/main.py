4f91c75cc7e8d79acdee90407e75aeb50cc0266b
Richard Shevchuk <shevchuk.richard.b@gmail.com>
2026-04-07 14:41:26 +0300

    Initial commit with basic functional

---
+++ app/main.py
@@ -0,0 +1,17 @@
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/health")
+def read_health():
+    return {"status": "healthy"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
